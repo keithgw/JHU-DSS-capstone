27da144d4c364d9924e4e39a98023391e6bbe06a
keithgw <keith.gordon.williams@gmail.com>
2015-08-06 14:12:43 -0400

    Updated server.R to handle punctuation

--- code/server.R
+++ code/server.R
@@ -1,10 +1,16 @@
+### To Do:
+# Back-off model
+# Smoothing Model <unk>
+# Tetragrams
+
+
 # load libraries
 library(stringr)
 library(stringi)
 library(dplyr)
 
 # load n-gram data.tables
-unigrams <- readRDS('unidat.rds')
+# unigrams <- readRDS('unidat.rds')
 bigrams <- readRDS('bidat.rds')
 trigrams <- readRDS('tridat.rds')
 
@@ -28,14 +34,32 @@ shinyServer(
         
         # create prediction object with interval from inputs
         predicted_word <- reactive({
-            sentence <- str_trim(input$input_txt) %>% 
-                str_split(" ")
+            # strip white space, remove punctuation, and tokenize input
+            trimmed <- str_trim(input$input_txt) %>% 
+                str_replace_all("[^A-Za-z ]", "")
+            sentence <- str_split(trimmed, " ")
+            
+            # pick 1-, 2-, 3-gram model based on length of sentence
             len <- length(sentence[[1]])
+            
+            # default prediction
             if (len < 1) {
                 return("the")
             }
+            
+            # 1 or 2 word input
             else if (len <= 2) {
-                return(extracti(ngram = input$input_txt, n = len + 1))
+                # concatenate tokens for input              
+                return(extracti(ngram = trimmed, n = len + 1))
+            }
+            
+            # more than 2 words
+            # input should be only last 2 words
+            else {
+                token <- str_c(sentence[[1]][len - 1], 
+                               sentence[[1]][len], 
+                               sep = " ")
+                return(extracti(ngram = token, n = 3))
             }
             })
                 
